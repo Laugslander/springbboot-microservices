bf80826b4910fa28547912dc3b8094f3d3b4409f
Dennis Geurts <1807523+wageurts@users.noreply.github.com>
2019-10-12 19:35:55 +0200

    Rename packages and classes

--- service-discovery/src/main/java/com/jbag/servicediscovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/jbag/servicediscovery/ServiceDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package org.svb.generiek.dmsservicediscovery;
+package com.jbag.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DmsServiceDiscoveryApplication {
+public class ServiceDiscoveryApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DmsServiceDiscoveryApplication.class, args);
+		SpringApplication.run(ServiceDiscoveryApplication.class, args);
 	}
 }
